a5a66221ffeed5a8c6ebaf9b8846969339c7937b
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-04-06 13:04:09 -0700

    changes

--- Player.cpp
+++ Player.cpp
@@ -3,8 +3,7 @@
 #include "Inventory.h"
 
 
-Player::Player()
-{
+Player::Player() {
 	Player::playerHealth = 100;
 	Inventory playerInventory();
 }
@@ -12,56 +11,46 @@ Player::Player()
 Player::~Player(){};
 
 //accessors
-std::string Player::getPlayerName() const
-{
+std::string Player::getPlayerName() const {
 	return playerName;
 }
 
-double Player::getPlayerHealth() const
-{
+double Player::getPlayerHealth() const {
 	return playerHealth;
 }
 
 //inventory accessors
-std::string Player::getInventorySlotItem(int invSlot) const
-{
+std::string Player::getInventorySlotItem(int invSlot) const {
 	return playerInventory.getInventorySlotItem(invSlot);
 }
 
-int Player::getInventorySlotQuantity(int invSlot) const
-{
+int Player::getInventorySlotQuantity(int invSlot) const {
 	return playerInventory.getInventorySlotQuantity(invSlot);
 }
 
-int Player::getInventorySlotQuantity(std::string itemName) const
-{
+int Player::getInventorySlotQuantity(std::string itemName) const {
 	return playerInventory.getInventorySlotQuantity(itemName);
 }
 
-void Player::displayInventory() const
-{
+void Player::displayInventory() const {
 	playerInventory.displayInventory();
 }
 
 //************************************************************************************************
 
 //mutators
-void Player::setPlayerName(std::string setName)
-{
+void Player::setPlayerName(std::string setName) {
 	Player::playerName = setName;
 }
 
-void Player::setPlayerHealth(double setHealth)
-{
+void Player::setPlayerHealth(double setHealth) {
 	Player::playerHealth = setHealth;
 }
 
-void Player::addToInventory(std::string item)
-{
+void Player::addToInventory(std::string item) {
 	playerInventory.addToInventory(item);
 }
 
-void Player::removeFromInventory(int invSlot)
-{
+void Player::removeFromInventory(int invSlot) {
 	playerInventory.removeFromInventory(invSlot);
 }
